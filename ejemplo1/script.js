be9e77e01a32b645ec705f4933be3cee404ecbf5
DiegoBritoSanchez <93787340+DiegoBritoSanchez@users.noreply.github.com>
2022-09-15 18:34:42 +0200

    Delete ejemplo1 directory

--- ejemplo1/script.js
+++
@@ -1,9 +0,0 @@
-const saludar = () => {
-  alert("Saludo de ejemplo");
-};
-const comensales = () => {
-  let comensal, papa;
-  comensal = window.prompt("¿Cuántos comensales van a ser?");
-  document.getElementById("comensalesL").innerHTML = comensal+' comensales';
-  console.log(1);
-};
