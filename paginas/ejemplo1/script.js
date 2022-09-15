c4416bdbdc9fa4f1fd66747f40fbdff04081e8bd
DiegoBritoSanchez <93787340+DiegoBritoSanchez@users.noreply.github.com>
2022-09-15 17:35:15 +0100

    Add files via upload

--- paginas/ejemplo1/script.js
+++ paginas/ejemplo1/script.js
@@ -1,3 +1,9 @@
 const saludar = () => {
   alert("Saludo de ejemplo");
 };
+const comensales = () => {
+  let comensal, papa;
+  comensal = window.prompt("¿Cuántos comensales van a ser?");
+  document.getElementById("comensalesL").innerHTML = comensal+' comensales';
+  console.log(1);
+};
