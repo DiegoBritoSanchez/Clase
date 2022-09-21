debe93ecd1794a7bfd4a94d81b3038708dd342dd
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-09-21 17:25:07 +0200

    a

--- paginas/ejemplo1/script.js
+++ paginas/ejemplo1/script.js
@@ -2,8 +2,18 @@ const saludar = () => {
   alert("Saludo de ejemplo");
 };
 const comensales = () => {
-  let comensal, papa;
-  comensal = window.prompt("¿Cuántos comensales van a ser?");
-  document.getElementById("comensalesL").innerHTML = comensal+' comensales';
-  console.log(1);
+  let comensal;
+  while (isNaN(comensal)) {
+    comensal = window.prompt("¿Cuántos comensales van a ser?");
+    comensal = comensal <= 0 ? 0 / 0 : comensal;
+    document.getElementById("comensalesL").innerHTML = comensal + " comensales";
+    if (comensal >= 2) {
+      document.getElementById("kg").innerHTML = (comensal * 500) / 1000 + " kg";
+    }
+  }
+};
+const fondo = () => {
+  const months = ["darkblue", "violet", "darkgoldenrod"];
+  const random = Math.floor(Math.random() * months.length);
+  document.body.style.backgroundColor = months[random];
 };
