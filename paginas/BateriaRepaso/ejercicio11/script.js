dbeaf1fdd6841fb47fbaa8301c2f9b703092cda8
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-09-23 16:16:23 +0200

    repaso

---
+++ paginas/BateriaRepaso/ejercicio11/script.js
@@ -0,0 +1,11 @@
+function ejercicio10() {
+    let doc = document.getElementById("piramide");
+    let docAux = "";
+    for (let index = 1; index <= 50; index++) {
+        for (let i2 = 1; i2 <= index; i2++) {
+            docAux += i2;
+        }
+        docAux += "<br>";
+    }
+    doc.innerHTML = docAux;
+}
