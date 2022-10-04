13fe92972a5244e9d99337b70a1fef6d4e90f56b
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-10-04 16:55:41 +0200

    martes

---
+++ paginas/Tema2/ejemplo1/script.js
@@ -0,0 +1,54 @@
+let listado = ["hola", "juego", "adivinar", "acierto", "probando"];
+let control;
+let tempo = 60;
+let muestra = document.getElementById("muestra");
+let solucion = document.getElementById("solucion");
+let entrada = document.getElementById("entrada");
+let tiempo = document.getElementById("tiempo");
+let intervalo;
+
+//Obtenemos una palabra aleatoria e la lista
+function aleatorio() {
+  if (!control) {
+    control = Math.floor(Math.random() * listado.length);
+    //Generamos los asteriscos que forman la palabra oculta
+    for (let index = 0; index < listado[control].length; index++) {
+      muestra.innerHTML += "<span id='span" + index + "'>*</span>";
+    }
+    intervalo = setInterval(temporizador, 500);
+  }
+}
+
+entrada.addEventListener("input", escribe);
+
+function escribe() {
+  let palabra = listado[control];
+  let span;
+
+  if (entrada.value == null) {
+    return;
+  }
+
+  for (let index = 0; index < palabra.length; index++) {
+    span = document.getElementById("span" + index);
+    if (entrada.value == palabra.charAt(index)) {
+      console.log(index);
+      console.log(entrada.value);
+      span.innerHTML = entrada.value;
+    }
+  }
+  entrada.value = "";
+  if (span.innerHTML != "*") {
+    clearInterval(intervalo);
+  }
+}
+
+function temporizador() {
+  if (tempo > 0) {
+    tiempo.innerHTML = tempo;
+    tempo--;
+  }
+  if (tempo == 0) {
+    alert("Te has quedado sin tiempo");
+  }
+}
