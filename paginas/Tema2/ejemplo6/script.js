47f691dd5da90b487c16ceb3fbb521c1cda8ae8d
DiegoBritoSanchez <diegobritosanchez13@gmail.com>
2022-10-18 18:29:58 +0100

    asd

--- paginas/Tema2/ejemplo6/script.js
+++ paginas/Tema2/ejemplo6/script.js
@@ -1,3 +1,4 @@
+//Primera parte
 function newWindow() {
     miVentana = window.open('popUpWindow.html', 'ejercicio', 'menubar=false, toolbar=false, location=false, directories=false, resizable=false, top=20, left=20');
     miVentana.focus();
@@ -14,5 +15,47 @@ function content() {
 
     //Google no funciona
     /*makeIframe("https://www.google.es/");*/
-    makeIframe("www.marca.com");
+    makeIframe("https://www.marca.com");
 }
+
+// Función para crear un iframe
+function makeIframe(page) {
+    // creamos el elemento
+    let iframe = document.createElement("iframe");
+    iframe.id = "iframe";
+    iframe.src = page;
+    iframe.style.width = '800px';
+    iframe.style.height = '600px';
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(iframe);
+}
+
+function closeWindow() {
+    // Cerramos esta ventana emergente
+    this.close();
+}
+
+//Segunda parte
+let nombre, surname, birth, birthClean, day, month, year;
+
+nombre = prompt("Introduce tu nombre:", "");
+
+while(!nombre || nombre=="" || nombre==null){
+    nombre=prompt("El nombre no puede estar en blanco.\nIntroduce tu nombre:", "");
+}
+
+surname = prompt("Introduce tu nombre:", "");
+
+while(!surname || surname=="" || surname==null){
+    nombre=prompt("El apellido no puede estar en blanco.\nIntroduce tu apellido:", "");
+}
+
+birth = prompt("Introduzca su fecha de nacimiento en formato DD/MM/YYYY:");
+birthclean = birth.split("/");
+day = parseInt(birth[0]);
+month = parseInt(birth[1]);
+year = parseInt(birth[2]);
+
+console.log(day);
+console.log(month);
+console.log(year);
