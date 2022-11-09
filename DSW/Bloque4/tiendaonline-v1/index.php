0199a675d4df9fef56d0174235d3a60e7db0dd2b
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-11-09 19:57:07 +0100

    jorge-miercoles

--- DSW/Bloque4/tiendaonline-v1/index.php
+++ DSW/Bloque4/tiendaonline-v1/index.php
@@ -2,16 +2,23 @@
 <?php include 'php/connect.inc' ?>
 <div class="container-sm justify-content-md-center ">
     <?php
-    echo "<table class='table table-dark table-striped-columns table-borderless table-responsive table-hover my-pad text-center'>";
-    $sql = "SELECT * from productos inner join imagenesproductos on productos.id = imagenesproductos.idproducto where existencias > 0 group by productos.id";
+    echo "<table class='table table-dark table-striped-columns table-borderless table-responsive table-hover my-pad text-center' id='productos'>";
+    $sql = "SELECT p.id pid, i.id, p.nombre, p.descripcion, p.precio, i.imagen from productos p inner join imagenesproductos i on p.id = i.idproducto where existencias > 0 group by p.id";
     $result = $conn->query($sql);
     echo "<tr><th>Imagen</th><th>Nombre</th><th>Descripición</th><th>Precio</th></tr>";
     $control = 0;
     while ($register = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr id='fila" . $control++ . "'><td><img src=photo/" . $register['imagen'] . " style='width:100px;'></td><td class='py-5'>" . $register['nombre'] . "</td><td class='py-5'>" . $register['descripcion'] . "</td><td class='py-5'>" . $register['precio'] . "</td></tr>";
+        echo "<tr id='fila" . $control++ . "'><td><a href = 'product.php?id=" . $register['pid'] . "'><img src=photo/" . $register['imagen'] . " style='width:100px;'></td>
+        <td class='py-5'><a href = 'product.php?id=" . $register['pid'] . "'>" . $register['nombre'] . "</td>
+        <td class='py-5'><a href = 'product.php?id=" . $register['pid'] . "'>" . $register['descripcion'] . "</td>
+        <td class='py-5'><a href = 'product.php?id=" . $register['pid'] . "'>" . $register['precio'] . "</td>
+        </tr>";
     }
     echo "</table>";
     ?>
+    <div>
+        <p id="res"></p>
+    </div>
 </div>
 
 <?php include 'php/footer.inc' ?>
---
+++ DSW/Bloque4/tiendaonline-v1/js/script.js
@@ -0,0 +1,6 @@
+// ! Conteo de filas
+for (let i = 0; i < productos.tBodies[0].rows.length - 1; i++) {
+    let filas = "fila";
+    filas += i;
+    
+}
