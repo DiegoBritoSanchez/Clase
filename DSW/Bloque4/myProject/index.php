3fc2e3d331055aa09f0580763041f5b77c50ee14
DiegoBritoSanchez <93787340+DiegoBritoSanchez@users.noreply.github.com>
2022-11-17 12:24:09 +0000

    x

--- DSW/Bloque4/myProject/index.php
+++ DSW/Bloque4/myProject/index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Store online</title>
 </head>
+
 <body>
-    <h1>prueba</h1>
     <?php
-    include ('php/connect.inc');
+    include('php/connect.inc');
     ?>
+    <h1>nline Stone Store</h1>
+
 </body>
+
 </html>
