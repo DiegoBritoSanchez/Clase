affb92992b604b3c09ea243785d3062e736cf6f0
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-11-21 19:47:05 +0100

    lunes-nico

--- DEW/Tema4/Formularios/Ejercicio2/js/script.js
+++ DEW/Tema4/Formularios/Ejercicio2/js/script.js
@@ -1,3 +1,12 @@
+// * Variables
+var card1;
+var card2;
+var control;
+var counter = 0;
+var divAux1;
+var divAux2;
+var success = 0;
+var error = 0;
 // * Funciones
 function setValues() {
     var values = Array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6);
@@ -15,21 +24,64 @@ function setValues() {
 }
 
 function comprobar(id) {
+    if (counter < 2) {
+        var div = "div" + id;
+        function setColors(value) {
+            var divValue = value - 1;
+            var colors = Array("red", "green", "black", "brown", "orange", "darkkhaki");
+            var randomColor = colors[divValue];
+            return randomColor;
+        }
 
-    var div = "div" + id;
+        if (typeof card1 === 'undefined') {
+            card1 = document.getElementById(div).getAttribute("value");
+            divValue = card1;
+            control = true;
+            divAux1 = "div" + id;
+        } else {
+            card2 = document.getElementById(div).getAttribute("value");
+            divValue = card2;
+            divAux2 = "div" + id;
+            control = true;
+        }
+        counter++;
+        if (control) {
+            document.getElementById(div).className += " rotate-center";
+            document.getElementById(div).style.backgroundColor = setColors(divValue);
+        }
 
-    function setColors(value) {
-        var divValue = value - 1;
-        var colors = Array("red", "green", "black", "brown", "orange", "darkkhaki");
-        var randomColor = colors[divValue];
-        return randomColor;
+        setTimeout(test, 2500);
+        console.log(card1);
+        console.log(card2);
+        function test() {
+            if (typeof card2 !== 'undefined') {
+
+                if (card1 == card2) {
+                    counter = 0;
+                    success++;
+                    document.getElementById("success").innerHTML = "Aciertos: " + success;
+                } else {
+                    counter = 0;
+                    card1 = undefined;
+                    card2 = undefined;
+                    error++;
+                    document.getElementById("fail").innerHTML = "Fallos: " + error;
+                    document.getElementById(divAux1).style.backgroundColor = "#6610f2";
+                    document.getElementById(divAux2).style.backgroundColor = "#6610f2";
+                    var flips = document.getElementsByClassName("rotate-center");
+
+                    for (let index = 0; index < flips.length; index++) {
+                        flips[index].classList.remove("rotate-center");
+                    }
+
+                }
+            }
+        }
+
+
     }
 
-    var divValue = document.getElementById(div).getAttribute("value");
 
-    document.getElementById(div).style.backgroundColor = setColors(divValue);
-
-    
 }
 
 // * Programa
