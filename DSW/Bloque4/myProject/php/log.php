b9d2aef0aeb0f8a9c717ee0853fea24bb701e1e6
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2022-11-30 16:06:15 +0100

    log-in Jorge

---
+++ DSW/Bloque4/myProject/php/log.php
@@ -0,0 +1,32 @@
+<?php
+$url = $_SERVER["REQUEST_URI"];
+$url == "/tienda/" ? $url = "./" : $url = "../";
+// $url == "/Clase/DSW/Bloque4/myProject/" ? $url = "./" : $url = "../";
+
+include($url . 'php/connect.inc');
+
+include($url . 'php/limpiar.inc');
+$usu = limpiar($_POST['email']);
+$psswd = limpiar($_POST['psswd']);
+$psswdHashed = hash('sha256', $psswd);
+
+$sql = 'SELECT * FROM users WHERE email = :e and psswd=:p';
+$result = $conn->prepare($sql);
+try {
+    $result->execute([
+        ':e' => $usu,
+        ':p' => $psswdHashed
+    ]);
+} catch (PDOException $e) {
+    $conn = null;
+    die('Fallo en la conexión a la BD: ' . $e->getMessage());
+}
+if ($result->rowCount() == 0) {
+    echo "Usuario y/o contraseñas incorrectos...";
+    die();
+} else {
+    $credentials = $result->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['user'] = $credentials['name'];
+    $_SESSION['type'] = $credentials['type'];
+    header('location: ../');
+}
--- DSW/Bloque4/myProject/php/login.php
+++ DSW/Bloque4/myProject/php/login.php
@@ -1,5 +1,5 @@
 <?php
-$url= $_SERVER["REQUEST_URI"];
+$url = $_SERVER["REQUEST_URI"];
 $url == "/tienda/" ? $url = "./" : $url = "../";
 // $url == "/Clase/DSW/Bloque4/myProject/" ? $url = "./" : $url = "../";
 
@@ -7,41 +7,25 @@ $url == "/tienda/" ? $url = "./" : $url = "../";
 include($url . 'php/connect.inc');
 // include($_SERVER["DOCUMENT_ROOT"] . '\Clase\DSW\Bloque4\myProject\php\header.inc');
 include($url . 'php/header.inc');
+include($url . 'php/limpiar.inc');
 
 ?>
 <div class="container-fluid py-5 loging" style=" background-color: rgba(0, 255, 136, 0.486); border-radius: 40px; margin-top:5%; width: 50%;">
-<form >
-  <!-- Email input -->
-  <div class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
-  </div>
+  <form action="log.php" method="POST">
+    <!-- Email input -->
+    <div class="form-outline mb-4">
+      <input type="email" id="email" class="form-control" name="email" />
+      <label class="form-label" for="email">Email address</label>
+    </div>
 
-  <!-- Password input -->
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
+    <!-- Password input -->
+    <div class="form-outline mb-4">
+      <input type="password" id="psswd" class="form-control" name="psswd" />
+      <label class="form-label" for="pswwd">Password</label>
+    </div>
+    <!-- Submit button -->
+    <div class="text-center"><input type="submit" value="Sign in" class="btn btn-white btn-block border border-dark rounded-pill"></div>
 
-  <!-- 2 column grid layout for inline styling -->
-  <!-- <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
 
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example31" style="margin-top: 10px;" checked />
-        <label class="form-check-label btn" for="form2Example31"> Remember me </label>
-      </div>
-    </div> -->
-
-    <!-- <div class="col ">
-      Simple link
-      <a href="#!" class="btn btn-white">Forgot password?</a>
-    </div> -->
-  <!-- </div> -->
-
-  <!-- Submit button -->
-  <div class="text-center"><button type="button" class="btn btn-white btn-block border border-dark rounded-pill">Sign in</button></div>
-  
-
-</form>
+  </form>
 </div>
