dbacd8f49a9750e8838c253f7c7f1a73f2c32ef6
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2023-02-06 19:55:11 +0100

    videos1-2

--- DEW/Tema9/proyecto1/src/App.js
+++ DEW/Tema9/proyecto1/src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import img0 from './images/rey_atanagildo.png';
+import img1 from './images/rey_ataulfo.png';
+import img2 from './images/rey_teodorico.png';
+import './style.css';
 
 function App() {
+  let names = ['Rey Atanagildo', 'Rey Ataulfo', 'Rey Teodorico'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nants ingonyama bagithi baba
-          Sithi uhm ingonyama
-          Nants ingonyama bagithi baba
-          Sithi uhm ingonyama
-          Siyo nqoba
-          Ingonyama nengw' enamabala...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <img src={img0} alt="Rey Atanagildo"/>
+        <div className="name">{names[0]}</div>
+      </div>
+      <div className="box">
+        <img src={img1} alt="Rey Ataulfo"/>
+        <div className="name">{names[1]}</div>
+      </div>
+      <div className="box">
+        <img src={img2} alt="Rey Teodorico"/>
+        <div className="name">{names[2]}</div>
+      </div>
     </div>
   );
 }
