47b4fd6d409e7505336cf0bf750e91c313049895
2DAW14 <2DAW14@DOMDPTOINFJMPP.INF>
2023-02-07 16:17:37 +0100

    proyecto2

--- DEW/Tema9/proyecto2/src/App.js
+++ DEW/Tema9/proyecto2/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import img from './images/rey_ervigio.png';
+import img2 from './images/rey_incognito.png';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const change = 23.16;
+  const boxRef = useRef();
+  const add = (e) => {
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+  }
+  const multiply = () => {
+    boxRef.current.innerHTML = Number(boxRef.current.innerHTML) * change;
+  }
+  const changeImg = (e) => {
+    e.target.src = img2;
+  }
+  const read = (e) => {
+    boxRef.current.innerHTML = e.target.value;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='box' onClick={add} ref={boxRef}>1</div>
+      <button onClick={multiply}>Confirm</button>
+      <div><img src={img} alt='' onClick={changeImg} /></div>
+      <input className='search' onChange={read} />
+    </>
   );
 }
 
